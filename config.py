05614a2ba5f76cd6715f3fd76150db502f4de30d
vsivav <82565682+vsivav@users.noreply.github.com>
2026-07-08 17:48:26 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,13 @@
+from pathlib import Path
 
+BASE_DIR = Path(__file__).parent
+
+DATA_DIR = BASE_DIR / "data"
+
+SQL_DIR = DATA_DIR / "sql"
+
+OUTPUT_DIR = DATA_DIR / "metadata"
+
+LOG_LEVEL = "INFO"
+
+SUPPORTED_DIALECT = "tsql"
