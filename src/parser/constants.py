7845199e885d289560334995dbac7b44bfd70c85
vsivav <82565682+vsivav@users.noreply.github.com>
2026-07-08 17:50:26 +0800

    Update constants.py

--- src/parser/constants.py
+++ src/parser/constants.py
@@ -1,1 +1,35 @@
+SUPPORTED_STATEMENTS = {
+    "SELECT",
+    "INSERT",
+    "UPDATE",
+    "DELETE",
+    "MERGE",
+    "CREATE",
+    "ALTER",
+    "DROP"
+}
 
+JOIN_TYPES = {
+    "JOIN",
+    "LEFT JOIN",
+    "RIGHT JOIN",
+    "FULL JOIN",
+    "INNER JOIN",
+    "CROSS JOIN"
+}
+
+AGGREGATE_FUNCTIONS = {
+    "SUM",
+    "COUNT",
+    "AVG",
+    "MIN",
+    "MAX"
+}
+
+WINDOW_FUNCTIONS = {
+    "ROW_NUMBER",
+    "RANK",
+    "DENSE_RANK",
+    "LEAD",
+    "LAG"
+}
