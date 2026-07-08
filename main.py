6773a320fd47d2671cdef0d9e6f5e7a0ff1710b7
vsivav <82565682+vsivav@users.noreply.github.com>
2026-07-08 18:34:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+from src.parser.parser import SQLParser
 
+if __name__ == "__main__":
+
+    parser = SQLParser()
+
+    ast = parser.parse_file("tests/sample.sql")
+
+    print("=" * 80)
+
+    print("Statements Parsed :", len(ast))
+
+    print("=" * 80)
+
+    for statement in ast:
+
+        print(statement)
+
+        print("-" * 80)
